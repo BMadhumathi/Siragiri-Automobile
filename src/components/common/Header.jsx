aea34059d53c8cce76c57ab9b124e81b8371fd43
BMadhumathi <bmadhumathi319@gmail.com>
2024-03-30 13:21:11 +0530

    Initial commit

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,28 @@
+import { Menu } from "@mui/icons-material"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import { navlink } from "../data/dummydata"
+import logo from "../data/images/logo.png"
+
+export const Header = () => {
+  const [responsive, setResponsive] = useState(false)
+  return (
+    <>
+      <header>
+        <div className='container flexsb'>
+         <p style={{paddingRight:450}}>Siragiri Vel Automobiles</p>
+          <div className={responsive ? "hideMenu" : "nav"}>
+            {navlink.map((links, i) => (
+              <Link to={links.url} key={i} data-aos='zoom-in-left'>
+                {links.text}
+              </Link>
+            ))}
+          </div>
+          <button className='toggle' onClick={() => setResponsive(!responsive)}>
+            <Menu className='icon' />
+          </button>
+        </div>
+      </header>
+    </>
+  )
+}
